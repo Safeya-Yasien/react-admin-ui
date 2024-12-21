8eac85a58cbe81939344a2007c9e6c7ec9d06992
Safeya-Yasien <ssafeya5@gmail.com>
2024-12-21 08:47:07 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-function App() {
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
 
+const Layout = () => {
   return (
-    <div>Hello World</div>
-  )
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contactContainer">
+          <Outlet />
+        </div>
+      </div>{" "}
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/users", element: <Users /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
